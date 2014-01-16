6034929c9887d3cbbfdab38ae8607cc83d0b6547
bigrpg <bigrpg@163.com>
2014-01-16 23:18:43 +0800

    modify

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,24 +4,39 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	//玩家数值
-	public float m_secret = 0;	//活动隐蔽性
-	public float m_power = 0;	//活动能力
+	public float m_secret = 0;	//活动隐蔽性[0--100]
+	public float m_power = 0;	//活动能力[0--100]
 	public float m_money = 0;	//金钱
 	public int m_support = 0; 	//追随者数量
 	public int m_death = 0;		//死亡人数
 
+	public int m_putdown = 0;	//政府镇压力度[0--100]
 
 
-	GUIText m_moneyText;
-	GUIText m_powerText;
+
+	GUIText m_moneyLabel;
+	GUIText m_powerLabel;
+	GUIText m_secretLabel;
+	GUIText m_supportLabel;
+	GUIText m_deathLabel;
+	GUITexture m_putdown_progress_tex;
 
 	// Use this for initialization
 	void Start () {
-		m_moneyText = this.transform.FindChild("money").GetComponent<GUIText>();
-		m_powerText = this.transform.FindChild("power").GetComponent<GUIText>();
+		m_moneyLabel = this.transform.FindChild("money_label").GetComponent<GUIText>();
+		m_powerLabel = this.transform.FindChild("power_label").GetComponent<GUIText>();
+		m_secretLabel = this.transform.FindChild("secret_label").GetComponent<GUIText>();
+		m_supportLabel = this.transform.FindChild("support_label").GetComponent<GUIText>();
+		m_deathLabel = this.transform.FindChild("death_label").GetComponent<GUIText>();
+		m_putdown_progress_tex = this.transform.FindChild("putdown_progress").GetComponent<GUITexture>();
 
 		SetMoney(99999);
 		SetPower(1);
+		SetSecret(100);
+		SetSupport(0);
+		SetDeath(0);
+		SetPutDown(20);
+
 	}
 	
 	// Update is called once per frame
@@ -31,12 +46,32 @@ public class GameManager : MonoBehaviour {
 
 	void SetMoney(int m) {
 		m_money = m;
-		m_moneyText.text = "金钱:" + m_money.ToString();
+		m_moneyLabel.text = "金钱:" + m_money.ToString();
 	}
 
 	void SetPower(int p) {
 		m_power = p;
-		m_powerText.text = "活动能力:" + m_power.ToString();
+		m_powerLabel.text = "斗争力:" + m_power.ToString();
 
+	}
+
+	void SetSecret(int s) {
+		m_secret = s;
+		m_secretLabel.text = "隐蔽度:" + m_secret.ToString();
+	}
+
+	void SetSupport(int s) {
+		m_support = s;
+		m_supportLabel.text = "追随者:" + m_support.ToString();
+	}
+
+	void SetDeath(int s) {
+		m_death = s;
+		m_deathLabel.text = "伤亡:" + m_death.ToString();
+	}
+
+	void SetPutDown(int p) {
+		m_putdown = p;
+		m_putdown_progress_tex.pixelInset=new Rect(0,-16,128*m_putdown/100,16);
 	}
 }
---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI()
+	{
+		// 文字大小
+		GUI.skin.label.fontSize = 48;
+		
+		// UI中心对齐
+		GUI.skin.label.alignment = TextAnchor.LowerCenter;
+		
+		// 显示标题
+		GUI.Label(new Rect(0, 30, Screen.width, 100), "秘密反抗");
+		
+		
+		// 开始游戏按钮
+		if (GUI.Button(new Rect(Screen.width * 0.5f - 100, Screen.height * 0.7f, 200, 30), "开始游戏"))
+		{
+			// 开始读取下一关
+			Application.LoadLevel("level1");
+		}
+	}
+}
